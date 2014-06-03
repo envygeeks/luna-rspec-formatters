b56c7a36ee0c1a407e7e2cbcd8e216590b38424e
Jordon Bedwell <envygeeks@gmail.com>
2014-06-03 10:34:21 -0500

    RSpec 3.0 support.

--- lib/luna/rspec/formatters/checks.rb
+++ lib/luna/rspec/formatters/checks.rb
@@ -1,20 +1,45 @@
-require_relative "base"
+require "rspec/core/formatters/base_text_formatter"
+require "rspec/version"
 
 module Luna
   module RSpec
     module Formatters
-      class Checks < Base
+      class Checks < ::RSpec::Core::Formatters::BaseTextFormatter
+        if ::RSpec::Version::STRING >= "3.0.0"
+          # Probably not available in the old version of RSpec.
+          ::RSpec::Core::Formatters.register self, :start, :start_dump, \
+            :example_passed, :example_pending, :example_failed
+        end
+
+        def start(*args)
+          super
+          output.puts
+        end
+
+        # ---------------------------------------------------------------------
+
+        def start_dump
+          super
+          output.puts
+        end
+
+        # ---------------------------------------------------------------------
+
         def example_passed(e)
           super(e)
           output.print("\s")
           output.print(success_color("\u2713"))
         end
 
+        # ---------------------------------------------------------------------
+
         def example_failed(e)
           super(e)
           output.print("\s")
           output.print(failure_color("\u2718"))
         end
+
+        # ---------------------------------------------------------------------
 
         def example_pending(e)
           super(e)
@@ -24,8 +49,4 @@ module Luna
       end
     end
   end
-end
-
-RSpec.configure do |config|
-  config.formatter = "Luna::RSpec::Formatters::Checks"
 end
--- lib/luna/rspec/formatters/documentation.rb
+++ lib/luna/rspec/formatters/documentation.rb
@@ -1,36 +1,57 @@
-require_relative "base"
+require "rspec/core/formatters/base_text_formatter"
+require "rspec/version"
 require "coderay"
 
 module Luna
   module RSpec
     module Formatters
-      class Documentation < Base
+      class Documentation < ::RSpec::Core::Formatters::BaseTextFormatter
+        if ::RSpec::Version::STRING >= "3.0.0"
+          # Probably not available in the old version of RSpec.
+          ::RSpec::Core::Formatters.register self, :start, :start_dump, \
+            :example_passed, :example_pending, :example_failed
+        end
+
+        def start(*args)
+          super
+          output.puts
+        end
+
+        # ---------------------------------------------------------------------
+
+        def start_dump
+          super
+          output.puts
+        end
+
+        # ---------------------------------------------------------------------
 
         def example_passed(e)
           super(e)
           print_description(e, :success)
         end
 
+        # ---------------------------------------------------------------------
+
         def example_failed(e)
           super(e)
           print_description(e, :failure)
         end
+
+        # ---------------------------------------------------------------------
 
         def example_pending(e)
           super(e)
           print_description(e, :pending)
         end
 
-        # -------------------------------------------------------------
+        # ---------------------------------------------------------------------
 
         def print_description(example, type)
-          output.print send(:"#{type}_color", "\t" + example.full_description + "\n")
+          output.print send(:"#{type}_color",
+            "\t" + example.full_description + "\n")
         end
       end
     end
   end
-end
-
-RSpec.configure do |config|
-  config.formatter = "Luna::RSpec::Formatters::Documentation"
 end
