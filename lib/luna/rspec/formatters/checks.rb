c1d6338fcd129e97fabdb08d6f03a5f0967b78a8
Jordon Bedwell <jordon@envygeeks.io>
2015-11-12 09:17:04 -0600

    Move around some files.

--- lib/luna/rspec/formatters/checks.rb
+++ lib/luna/rspec/formatters/checks.rb
@@ -1,11 +1,9 @@
-require "rspec/core/formatters/base_text_formatter"
-require "luna/rspec/formatters/profile"
-require "rspec/version"
+require "luna/rspec/formatters/emoji"
 
 module Luna
   module RSpec
     module Formatters
-      class Checks < ::RSpec::Core::Formatters::BaseTextFormatter
+      class Checks < Emoji
         include Profile
 
         if Gem::Version.new(::RSpec::Version::STRING) >= Gem::Version.new("3.0")
@@ -19,50 +17,25 @@ module Luna
           ]
         end
 
-        [:success, :failure, :pending].each do |m|
-          define_method "#{m}_color" do |v|
-            defined?(super) ? super(v) : \
-              ::RSpec::Core::Formatters::ConsoleCodes.wrap(v, m)
-          end
-        end
-
-        def allowed_cols
-          @cols ||= (`tput cols 2>/dev/null || 80`.to_i / 1.6).floor
-        end
-
-        def start(*args)
-          @lines = 0
-          output.puts
-        end
-
-        def start_dump(*args)
-          output.puts
-        end
-
         def example_passed(e)
           newline_or_addup
-          output.print "".freeze, success_color(
+          output.print " ".freeze, success_color(
             "\u2714"
           )
         end
 
         def example_failed(e)
           newline_or_addup
-          output.print "".freeze, failure_color(
+          output.print " ".freeze, failure_color(
             "\u2718"
           )
         end
 
         def example_pending(e)
           newline_or_addup
-          output.print "".freeze, pending_color(
+          output.print " ".freeze, pending_color(
             "\u203D"
           )
-        end
-
-        private
-        def newline_or_addup
-          @lines == allowed_cols ? (output.puts; @lines = 1) : @lines+=1
         end
       end
     end
---
+++ lib/luna/rspec/formatters/emoji.rb
@@ -0,0 +1,38 @@
+require "rspec/core/formatters/base_text_formatter"
+require "luna/rspec/formatters/profile"
+require "rspec/version"
+
+module Luna
+  module RSpec
+    module Formatters
+      class Emoji < ::RSpec::Core::Formatters::BaseTextFormatter
+        include Profile
+
+        [:success, :failure, :pending].each do |m|
+          define_method "#{m}_color" do |v|
+            defined?(super) ? super(v) : \
+              ::RSpec::Core::Formatters::ConsoleCodes.wrap(v, m)
+          end
+        end
+
+        def allowed_cols
+          @cols ||= (`tput cols 2>/dev/null || 80`.to_i / 1.6).floor
+        end
+
+        def start(*args)
+          @lines = 0
+          output.puts
+        end
+
+        def start_dump(*args)
+          output.puts
+        end
+
+        private
+        def newline_or_addup
+          @lines == allowed_cols ? (output.puts; @lines = 1) : @lines+=1
+        end
+      end
+    end
+  end
+end
